4eb452d974a3ddb3a3aabf33ffe66a726aadc6de
Jianfei Hu <jianfeih@google.com>
2018-03-08 11:19:08 -0800

    add yaml.

--- writer/build.sh
+++ writer/build.sh
@@ -2,3 +2,7 @@
 
 echo "Building hostpath-writer image..."
 docker build -t hostpath-writer .
+
+echo "Pushing to gcr..."
+docker tag hostpath-writer gcr.io/jianfeih-test/hostpath-writer
+gcloud docker -- push gcr.io/jianfeih-test/hostpath-writer
